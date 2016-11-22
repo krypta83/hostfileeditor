edd4eb8a8fe26f2423c8d16868de556934ed8671
sr <sr@BALZ-NB-218>
2016-11-22 08:38:17 +0100

    Projektdateien hinzufügen.

---
+++ Host-File Editor/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Host_File_Editor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmdSpeichern = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgv
+            // 
+            this.dgv.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(12, 62);
+            this.dgv.Name = "dgv";
+            this.dgv.Size = new System.Drawing.Size(779, 433);
+            this.dgv.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Open Sans", 8.25F);
+            this.label1.Location = new System.Drawing.Point(12, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Inhalt";
+            // 
+            // cmdSpeichern
+            // 
+            this.cmdSpeichern.Font = new System.Drawing.Font("Open Sans", 8.25F);
+            this.cmdSpeichern.Location = new System.Drawing.Point(12, 501);
+            this.cmdSpeichern.Name = "cmdSpeichern";
+            this.cmdSpeichern.Size = new System.Drawing.Size(75, 23);
+            this.cmdSpeichern.TabIndex = 5;
+            this.cmdSpeichern.Text = "Speichern";
+            this.cmdSpeichern.UseVisualStyleBackColor = true;
+            this.cmdSpeichern.Click += new System.EventHandler(this.cmdSpeichern_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(803, 545);
+            this.Controls.Add(this.cmdSpeichern);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgv);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button cmdSpeichern;
+    }
+}
+
---
+++ Host-File Editor/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Host_File_Editor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataGridViewCheckBoxColumn chk = new DataGridViewCheckBoxColumn();
+            dgv.Columns.Add(chk);
+            chk.HeaderText = "#";
+            dgv.Columns.Add("spIP", "IP");
+            dgv.Columns.Add("spName", "Name");
+            dgv.Columns[2].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+
+            //Breite auf 100%
+            //this.dgv.Columns[2].AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+
+            string path = @"C:\Windows\System32\drivers\etc\hosts";
+            string[] s = File.ReadAllLines(path);
+            foreach (string x in s)
+            {
+                string y = x.Replace("\t", " ");
+                while (y.IndexOf("  ") > 0)
+                {
+                    y = y.Replace("  ", " ");
+                }
+                string[] lines = y.Split(' ');
+
+                bool status = true;
+                string ip = lines[0];
+                string host = lines[1];
+
+                if(ip == "#")
+                {
+                    status = false;
+                    ip = lines[1];
+                    host = lines[2];
+                }
+                dgv.Rows.Add(status, ip, host);
+
+            }
+        }
+        private void cmdSpeichern_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
